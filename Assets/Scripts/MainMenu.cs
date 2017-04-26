ed381a5dddbd8067f547772038c40745cd2531bc
Alex Lee <adlee520@gmail.com>
2017-04-26 03:04:17 -0400

    menus screens created

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,9 +10,28 @@ public class MainMenu : MonoBehaviour {
 	public bool isMedium;
 	public bool isHard;
 	public bool isBack;
+	public bool isPuzzles = false;
+	public bool isPuzzle1 = false;
+	public bool isPuzzle2 = false;
 
 	void OnMouseUp(){
 		if (isSinglePlayer) {
+			if (isEasy && (!isPuzzle1 || !isPuzzle2) ) {
+				Application.LoadLevel (3);
+				GetComponent<Renderer> ().material.color = Color.cyan;
+			}
+			if (isMedium && (!isPuzzle1 || !isPuzzle2) ) {
+				Application.LoadLevel (4);
+				GetComponent<Renderer> ().material.color = Color.cyan;
+			}
+			if (isHard && (!isPuzzle1 || !isPuzzle2) ) {
+				Application.LoadLevel (5);
+				GetComponent<Renderer> ().material.color = Color.cyan;
+			}
+			if (isBack) {
+				Application.LoadLevel (0);
+				GetComponent<Renderer> ().material.color = Color.cyan;
+			}
 			Application.LoadLevel (1);
 			GetComponent<Renderer> ().material.color = Color.cyan;
 		}
@@ -20,22 +39,41 @@ public class MainMenu : MonoBehaviour {
 			Application.Quit ();
 			GetComponent<Renderer> ().material.color = Color.cyan;
 		}
-		if (isEasy) {
+		if (isPuzzles) {
+			if (isPuzzle1) {
+				if (isEasy && (isPuzzle1 || isPuzzle2)){
+					Application.LoadLevel (8);
+					GetComponent<Renderer> ().material.color = Color.cyan;
+				}
+				if (isMedium && (isPuzzle1 || isPuzzle2) ) {
+					Application.LoadLevel (9);
+					GetComponent<Renderer> ().material.color = Color.cyan;
+				}
+				if (isHard && (isPuzzle1 || isPuzzle2) ) {
+					Application.LoadLevel (10);
+					GetComponent<Renderer> ().material.color = Color.cyan;
+				}
+				Application.LoadLevel (6);
+				GetComponent<Renderer> ().material.color = Color.cyan;
+			}
+			if (isPuzzle2) {
+				if (isEasy && (isPuzzle1 || isPuzzle2)){
+					Application.LoadLevel (8);
+					GetComponent<Renderer> ().material.color = Color.cyan;
+				}
+				if (isMedium && (isPuzzle1 || isPuzzle2) ) {
+					Application.LoadLevel (9);
+					GetComponent<Renderer> ().material.color = Color.cyan;
+				}
+				if (isHard && (isPuzzle1 || isPuzzle2) ) {
+					Application.LoadLevel (10);
+					GetComponent<Renderer> ().material.color = Color.cyan;
+				}
+				Application.LoadLevel (7);
+				GetComponent<Renderer> ().material.color = Color.cyan;
+			}
 			Application.LoadLevel (2);
 			GetComponent<Renderer> ().material.color = Color.cyan;
 		}
-		if (isMedium) {
-			Application.LoadLevel (3);
-			GetComponent<Renderer> ().material.color = Color.cyan;
-		}
-		if (isHard) {
-			Application.LoadLevel (4);
-			GetComponent<Renderer> ().material.color = Color.cyan;
-		}
-		if (isBack) {
-			Application.LoadLevel (0);
-			GetComponent<Renderer> ().material.color = Color.cyan;
-		}
-	
 	}
 }
