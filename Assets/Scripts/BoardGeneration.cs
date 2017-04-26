4f8f024d336b029fb2316e8abf84295c5bab5cfd
LiraLamine <bayon.arguu@gmail.com>
2017-04-26 00:02:11 -0400

    finished up the two puzzles

--- Assets/Scripts/BoardGeneration.cs
+++ Assets/Scripts/BoardGeneration.cs
@@ -78,38 +78,16 @@ public class BoardGeneration : MonoBehaviour {
 
 	public void Puzzle2()
 	{
-		board.generatePiece(Board.PlayerE.White, new Point(0, 0), Piece.PieceTypeE.ROOK, board.whiteRook, "Rook");
-		board.generatePiece(Board.PlayerE.White, new Point(0, 1), Piece.PieceTypeE.KNIGHT, board.whiteKnight, "Knight");
-		board.generatePiece(Board.PlayerE.White, new Point(0, 2), Piece.PieceTypeE.BISHOP, board.whiteBishop, "Bishop");
-		board.generatePiece(Board.PlayerE.White, new Point(0, 3), Piece.PieceTypeE.QUEEN, board.whiteQueen, "Queen");
-		board.generatePiece(Board.PlayerE.White, new Point(0, 4), Piece.PieceTypeE.KING, board.whiteKing, "King");
-		board.generatePiece(Board.PlayerE.White, new Point(0, 5), Piece.PieceTypeE.BISHOP, board.whiteBishop, "Bishop");
-		board.generatePiece(Board.PlayerE.White, new Point(0, 6), Piece.PieceTypeE.KNIGHT, board.whiteKnight, "Knight");
-		board.generatePiece(Board.PlayerE.White, new Point(0, 7), Piece.PieceTypeE.ROOK, board.whiteRook, "Rook");
-		board.generatePiece(Board.PlayerE.White, new Point(1, 0), Piece.PieceTypeE.PAWN, board.whitePawn, "Pawn");
+		board.generatePiece(Board.PlayerE.White, new Point(2, 5), Piece.PieceTypeE.ROOK, board.whiteRook, "Rook");
+		board.generatePiece(Board.PlayerE.White, new Point(1, 4), Piece.PieceTypeE.KING, board.whiteKing, "King");
 		board.generatePiece(Board.PlayerE.White, new Point(1, 1), Piece.PieceTypeE.PAWN, board.whitePawn, "Pawn");
-		board.generatePiece(Board.PlayerE.White, new Point(1, 2), Piece.PieceTypeE.PAWN, board.whitePawn, "Pawn");
-		board.generatePiece(Board.PlayerE.White, new Point(1, 3), Piece.PieceTypeE.PAWN, board.whitePawn, "Pawn");
-		board.generatePiece(Board.PlayerE.White, new Point(1, 4), Piece.PieceTypeE.PAWN, board.whitePawn, "Pawn");
 		board.generatePiece(Board.PlayerE.White, new Point(1, 5), Piece.PieceTypeE.PAWN, board.whitePawn, "Pawn");
-		board.generatePiece(Board.PlayerE.White, new Point(1, 6), Piece.PieceTypeE.PAWN, board.whitePawn, "Pawn");
-		board.generatePiece(Board.PlayerE.White, new Point(1, 7), Piece.PieceTypeE.PAWN, board.whitePawn, "Pawn");
+		board.generatePiece(Board.PlayerE.White, new Point(2, 7), Piece.PieceTypeE.PAWN, board.whitePawn, "Pawn");
 
-		board.generatePiece(Board.PlayerE.Black, new Point(6, 0), Piece.PieceTypeE.PAWN, board.blackPawn, "Pawn");
-		board.generatePiece(Board.PlayerE.Black, new Point(6, 1), Piece.PieceTypeE.PAWN, board.blackPawn, "Pawn");
-		board.generatePiece(Board.PlayerE.Black, new Point(6, 2), Piece.PieceTypeE.PAWN, board.blackPawn, "Pawn");
-		board.generatePiece(Board.PlayerE.Black, new Point(6, 3), Piece.PieceTypeE.PAWN, board.blackPawn, "Pawn");
-		board.generatePiece(Board.PlayerE.Black, new Point(6, 4), Piece.PieceTypeE.PAWN, board.blackPawn, "Pawn");
-		board.generatePiece(Board.PlayerE.Black, new Point(6, 5), Piece.PieceTypeE.PAWN, board.blackPawn, "Pawn");
-		board.generatePiece(Board.PlayerE.Black, new Point(6, 6), Piece.PieceTypeE.PAWN, board.blackPawn, "Pawn");
-		board.generatePiece(Board.PlayerE.Black, new Point(6, 7), Piece.PieceTypeE.PAWN, board.blackPawn, "Pawn");
-		board.generatePiece(Board.PlayerE.Black, new Point(7, 0), Piece.PieceTypeE.ROOK, board.blackRook, "Rook");
-		board.generatePiece(Board.PlayerE.Black, new Point(7, 1), Piece.PieceTypeE.KNIGHT, board.blackKnight, "Knight");
-		board.generatePiece(Board.PlayerE.Black, new Point(7, 2), Piece.PieceTypeE.BISHOP, board.blackBishop, "Bishop");
-		board.generatePiece(Board.PlayerE.Black, new Point(7, 3), Piece.PieceTypeE.QUEEN, board.blackQueen, "Queen");
-		board.generatePiece(Board.PlayerE.Black, new Point(7, 4), Piece.PieceTypeE.KING, board.blackKing, "King");
-		board.generatePiece(Board.PlayerE.Black, new Point(7, 5), Piece.PieceTypeE.BISHOP, board.blackBishop, "Bishop");
-		board.generatePiece(Board.PlayerE.Black, new Point(7, 6), Piece.PieceTypeE.KNIGHT, board.blackKnight, "Knight");
-		board.generatePiece(Board.PlayerE.Black, new Point(7, 7), Piece.PieceTypeE.ROOK, board.blackRook, "Rook");
+		board.generatePiece(Board.PlayerE.Black, new Point(5, 8), Piece.PieceTypeE.PAWN, board.blackPawn, "Pawn");
+		board.generatePiece(Board.PlayerE.Black, new Point(4, 6), Piece.PieceTypeE.PAWN, board.blackPawn, "Pawn");
+		board.generatePiece(Board.PlayerE.Black, new Point(3, 7), Piece.PieceTypeE.PAWN, board.blackPawn, "Pawn");
+		board.generatePiece(Board.PlayerE.Black, new Point(1, 6), Piece.PieceTypeE.ROOK, board.blackRook, "Rook");
+		board.generatePiece(Board.PlayerE.Black, new Point(3, 1), Piece.PieceTypeE.KING, board.blackKing, "King");
 	}
 }
